28ac9a8876010f77e440337eb6f62ddda5b35b0a
John C. Anderson <SSTattler11@gmail.com>
2014-02-04 07:36:54 -0700

    Add prop_permutation, prop_maximum, prop_append

--- quickSort.hs
+++ quickSort.hs
@@ -2,7 +2,7 @@ import Test.QuickCheck
 import Data.List
 
 
--- QQQQQQQ
+--
 -- quickSort 
 --
 quickSort :: (Ord a) => [a] -> [a]
@@ -16,12 +16,26 @@ quickSort (x:rest) =
  
 
 prop_idempotent xs = quickSort (quickSort xs) == quickSort xs
+
 prop_minimum xs    = not (null xs) ==> head (quickSort xs) == minimum xs
+
 prop_ordered xs    = ordered (quickSort xs)
                         where ordered []       = True
                               ordered [x]      = True
                               ordered (x:y:xs) = x <= y && ordered (y:xs)
-                        
+
+prop_permutation xs = permutation xs (quickSort xs)
+   where permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
+
+prop_maximum xs = not (null xs) ==> last (quickSort xs) == maximum xs
+
+prop_append xs ys = 
+   not (null xs) ==> 
+      not (null ys) ==> 
+         head (quickSort (xs ++ ys)) == min (minimum xs) (minimum ys)
+
+
+
 
 -- prop_idempotent []
 -- 
