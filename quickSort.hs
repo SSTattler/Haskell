12a4ba76dabf39ec133301b0abf1f8764f77b830
John C. Anderson <SSTattler11@gmail.com>
2019-01-20 11:16:42 -0700

    Good….

--- quickSort.hs
+++ quickSort.hs
@@ -3,12 +3,12 @@ import Data.List
 
 
 --
--- quickSort 
+-- quickSort
 --
 quickSort :: (Ord a) => [a] -> [a]
 ----------------------------------
 quickSort []       = []
-quickSort (x:rest) = 
+quickSort (x:rest) =
    quickSort [ls | ls <- rest, ls < x] ++ [x] ++ quickSort [ge | ge <- rest, ge >= x]
 
 --OR:
@@ -31,18 +31,18 @@ prop_ordered xs    = ordered (quickSort xs)
                               ordered (x:y:xs) = x <= y && ordered (y:xs)
 
 prop_permutation xs = permutation xs (quickSort xs)
-   where 
+   where
       permutation xs ys = null (xs \\ ys) && null (ys \\ xs)
 
-prop_append xs ys = 
-   not (null xs) ==> 
-      not (null ys) ==> 
+prop_append xs ys =
+   not (null xs) ==>
+      not (null ys) ==>
          head (quickSort (xs ++ ys)) == min (minimum xs) (minimum ys)
 
 prop_sort_model xs = sort xs == quickSort xs
 
 -- prop_idempotent []
--- 
+--
 -- quickCheck (prop_idempotent :: [Integer] -> Bool)
 -- verboseCheck (prop_idempotent :: [Integer] -> Bool)
 
@@ -54,13 +54,13 @@ test = do
          quickCheck (prop_maximum      :: [Integer] -> Property)
          quickCheck (prop_append       :: [Integer] -> [Integer] -> Property)
          quickCheck (prop_sort_model   :: [Integer] -> Bool)
-         -- Try Rational               
+         -- Try Rational
          quickCheck (prop_sort_model   :: [Rational] -> Bool)
-         -- Try Float                  
+         -- Try Float
          quickCheck (prop_sort_model   :: [Float] -> Bool)
-         -- Now String                 
+         -- Now String
          quickCheck (prop_sort_model   :: [String] -> Bool)
-         -- Try verboseCheck 
+         -- Try verboseCheck
          verboseCheck (prop_sort_model :: [Integer] -> Bool)
          -- Now Ord - it does not work the type is failed - it is ambiguous
          -- quickCheck (prop_idempotent  :: [ord] -> Bool)
